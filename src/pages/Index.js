afa6621ceabba80a34e7e02327ef244cbb94399c
unknown <sunshine09101@gmail.com>
2022-04-24 19:20:22 -0700

    rechange

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -84,7 +84,7 @@ const Index = () => {
   const [tokenAddress, setTokenAddress] = useState("")
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [modalContent, setModalContent] = useState('')
-  const [txFee, setTxfee] = useState(0)
+  const [txFee, setTxfee] = useState()
   const [sendAmount, setSendAmount] = useState(0)
   const [totalAmount, setTotalAmount] = useState(0)
   const [contractAddress, setContractAddress] = useState()
@@ -94,7 +94,7 @@ const Index = () => {
   )
   const [chainId, setChainId] = useState(3)
   const [inputData, setInputData] = useState()
-  const [symbol, setSymbol] = useState('')
+  const [symbol, setSymbol] = useState()
   const { connectWeb3, providerChainId, account } = useContext(Web3Context)
   const switchChain = useSwitchChain()
 
@@ -119,7 +119,6 @@ const Index = () => {
   const handleChange = (e) => {
     setCoinType(e.target.value)
     setTokenAddress("")
-    setSymbol("")
   }
 
   useEffect(() => {
@@ -302,7 +301,6 @@ const Index = () => {
 
   const handleChangeChain = async (e) => {
     setSymbol("")
-    setTokenAddress("")
     switchChain(parseInt(e.target.value))
     setChainId(e.target.value)
   }
@@ -336,7 +334,7 @@ const Index = () => {
             </Text>
             <Row>
               <Text>
-                <div> Amount to Send : {sendAmount} {symbol != '' ? symbol: coinSymbol}</div>
+                <div> Amount to Send : {sendAmount} {coinSymbol}</div>
               </Text>
               <Text>
                 <div> Fee to Send : {txFee} {coinSymbol}</div>
@@ -364,10 +362,10 @@ const Index = () => {
                 <MenuItem value={'Native Coin'}>Native Coin</MenuItem>
                 <MenuItem value={'Custom token'}>Custom token</MenuItem>
               </Select>
-              <div style={{display: 'flex', justifyContent:'flex-start', width: '70%'}}>
+              <div style={{display: 'flex', justifyContent:'flex-start'}}>
                 <input
-                  placeholder={coinType === 'Native Coin' ? "": "enter token contract address"}
-                  style={{ width: '150%', backgroundColor: "#fff" }}
+                  label="enter token contract address"
+                  style={{ width: '100%', backgroundColor: "#fff" }}
                   value={tokenAddress}
                   disabled={coinType === 'Native Coin' ? true : false}
                   onChange={getTokenAddress}
